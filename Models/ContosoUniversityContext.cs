92777f94c897e0e628b433879adee953200c4854
Will <doggy.huang@gmail.com>
2021-10-15 10:27:21 +0800

    完成 Entity Framework Core 初始化作業

--- Models/ContosoUniversityContext.cs
+++ Models/ContosoUniversityContext.cs
@@ -32,8 +32,6 @@ namespace a1.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ContosoUniversity;Trusted_Connection=True;MultipleActiveResultSets=true");
             }
         }
 
